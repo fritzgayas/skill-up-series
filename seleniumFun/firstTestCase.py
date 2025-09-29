2465009b188402bf0ee1574502b4b1fcb2f033f6
Fritz <fritzgayas.tech@gmail.com>
2025-09-29 11:01:47 +0800

    Ignore .vscode and seleniumFun

--- seleniumFun/firstTestCase.py
+++
@@ -1,8 +0,0 @@
-# .\.venv\Scripts\activate
-# pip install selenium
-# pip install webdriver-manager
-
-from selenium.webdriver import Chrome
-from time import time
-driver = Chrome()
-driver.get("http://127.0.0.1:5500/htmlProject/contact.html")
