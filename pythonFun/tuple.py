009e0c8d61ab57bf504af36ef4c07784a356536e
Fritz <fritzgayas.tech@gmail.com>
2025-10-04 23:58:31 +0800

    add concatenate tuples

--- pythonFun/tuple.py
+++ pythonFun/tuple.py
@@ -1,7 +1,9 @@
 # Tuple in python
-# Tuple is only possible for read operations, unlike List who can do read/write
+# Tuple is only possible for read operations, unlike List which can do read/write
+# Tuple does not support update, insert, and remove of elements
 
-secretCodes = ['Alpha', 'Beta', 'Gamma', 'Delta', 'Echo']
+secretCodes = ['Alpha', 'Bravo', 'Charlie', 'Delta', 'Echo']
+secretCodes_updated = ['Foxtrot', 'Golf']
 tuple_length = len(secretCodes)
 
 # A. Fetch data from tuple approach 1
@@ -10,6 +12,11 @@ for i in range(0, tuple_length):
     print(secretCodes[i])
 
 # B. Fetch data from tuple approach 2
-print("********** This is the second appraoch **********")
+print("********** This is the second approach **********")
 for data in secretCodes:
     print(data)
+
+# C. Concatenate Tuples
+concatenatedTuple = secretCodes + secretCodes_updated
+print("********** Result after concatenation **********")
+print(concatenatedTuple)
