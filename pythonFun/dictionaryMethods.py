7d4e5ce3361709a22d9e3875df8e53c73f3d6226
Fritz <fritzgayas.tech@gmail.com>
2025-10-05 00:22:01 +0800

    add dictionaryMethods

---
+++ pythonFun/dictionaryMethods.py
@@ -0,0 +1,8 @@
+# Dictionary in python
+# Common methods can be used with dictionary includes keys, values, items, find length in dictionary
+
+DictyTutorial = {"K1":"Val1", 2:"Val2", "K3": 2025}
+print(DictyTutorial.keys()) #print all the keys
+print(DictyTutorial.values()) #print all the values
+print(DictyTutorial.items()) #print all key-value pairs
+print(len(DictyTutorial)) #print the number of pairs
