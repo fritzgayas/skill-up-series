871ed1b914607b680cdb955e08eb7f53f448ea9c
Fritz <fritzgayas.tech@gmail.com>
2025-10-09 22:47:08 +0800

    Update checkPalindrome

--- codingInterviewFun/5_checkPalindrome.py
+++ codingInterviewFun/5_checkPalindrome.py
@@ -1,5 +1,5 @@
 # ============================================
-# 4_checkPalindrome
+# 5_checkPalindrome
 # ============================================
 # Instruction:
 # Write a program that checks whether a given string is a palindrome.
